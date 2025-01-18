d379b14fbf95c4900ba26c70aef184ccfe3a4cd0
NgHgTan <157337651+NgHgTan@users.noreply.github.com>
2025-01-18 16:42:30 +0700

    fix build error

--- src/components/chart/Scatter.tsx
+++ src/components/chart/Scatter.tsx
@@ -16,9 +16,9 @@ type ScatterChartProps = {
 const ScatterChart: React.FC<ScatterChartProps> = ({ columns }) => {
   const numericColumns = columns.filter(
     (col) => col.dataType === EDataType.NUMBER
-  ) as Extract<DataColumn, { dataType: EDataType.NUMBER }>[];
+  ) as DataColumn[];
 
-  const datasets = numericColumns.map((col, index) => {
+  const datasets = numericColumns.map((col) => {
     const randomColor = getRandomColor()
     return {
       label: col.name,
